2f9832ad0f7c0edcbfaad81abc698a84156b2102
syb8200 <syb8200@naver.com>
2022-07-14 03:10:26 +0900

    [Login] input 구현 및 local storage 사용

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-const loginInput = loginForm.querySelector("#login-form input");
-const loginButton = loginForm.querySelector("#login-form button");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick(){
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+function onLoginSubmit(event){
+    event.preventDefault();
     const username = loginInput.value;
-    console.log(username);
+    localStorage.setItem(USERNAME_KEY, username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreeting(username);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+function paintGreeting(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername == null){
+    //form 이 보여지는 부분
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else{
+    //form 이 사라지는 부분
+    paintGreeting(savedUsername);
+}
